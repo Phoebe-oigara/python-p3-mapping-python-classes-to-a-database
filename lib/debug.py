1e445113b205b89ed98f7977a2680e61398d9504
Phoebe Oigara <phoebeoigara@gmail.com>
2023-06-01 18:50:07 +0300

    experimenting withSQLalchemy

--- lib/debug.py
+++ lib/debug.py
@@ -3,6 +3,16 @@
 from config import CONN, CURSOR
 from song import Song
 
-
 if __name__ == '__main__':
-    import ipdb; ipdb.set_trace()
+    Song.create_table()
+
+    CURSOR.execute("PRAGMA table_info(songs)").fetchall()
+
+    hello = Song("Hello", "25")
+    hello.save()
+
+    despacito = Song("Despacito", "Vida")
+    despacito.save()
+
+    CONN.commit()
+    CONN.close()
